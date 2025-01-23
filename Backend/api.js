c65178670ce24caabb4e99f1b32401da38b6fc99
EspinAndres01 <andresboki2000@gmail.com>
2025-01-23 03:59:19 -0500

    conect Front api

--- Backend/api.js
+++ Backend/api.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = "https://awd-22962024-the-bug-slayer.vercel.app/";  // Cambia esto según tu entorno
+const API_BASE_URL = "https://awd-22962024-the-bug-slayer.vercel.app";  // Cambia esto según tu entorno
 module.exports = API_BASE_URL;
